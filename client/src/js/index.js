b1ef9f7c8b9db5c8363cdd3584d08654775a7d77
Argent <jrargent42@gmail.com>
2022-11-30 17:23:21 -0500

    add indexed db

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,6 @@
 import "./form";
 import "./submit";
+import { initdb } from "./database";
 import "../css/index.css";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,6 +10,7 @@ import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
 window.addEventListener("load", function () {
+  initdb();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
