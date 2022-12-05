cfacf155d577c35884568ad1af7571edb1517cf4
Argent <jrargent42@gmail.com>
2022-12-05 11:40:10 -0500

    add pwa installation

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -27,6 +27,21 @@ newContactButton.addEventListener("click", (event) => {
   toggleForm();
 });
 
+const installBtn = document.getElementById("installBtn");
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = "visible";
+
+  installBtn.addEventListener("click", () => {
+    event.prompt();
+    installBtn.setAttribute("disabled", true);
+    installBtn.textContent = "Installed!";
+  });
+});
+window.addEventListener("appinstalled", (event) => {
+  console.log("👍", "appinstalled", event);
+});
+
 form.addEventListener("submit", (event) => {
   // Handle data
   event.preventDefault();
